2eee24b8da7677ec1eeba2ac12b5fead1f9d8af5
Henrik <henrik@henrikmdev.com>
2025-08-04 22:21:44 -0400

    Add prompt to ask for name

--- main.c
+++ main.c
@@ -2,6 +2,13 @@
 #include <stdio.h>
 
 int main() {
+    char name[50];
+
     printf("Hello, world!\n");
+
+    printf("Enter your name: ");
+    fgets(name, sizeof(name), stdin);
+
+    printf("Hello, %s", name);
     return 0;
 }
